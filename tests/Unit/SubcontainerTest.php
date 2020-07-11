09fef48af124ba1e837692f6a988434bcefb2a30
Simon Hampel <simon@hampelgroup.com>
2020-07-11 15:32:09 +1000

    unit tests

---
+++ tests/Unit/SubcontainerTest.php
@@ -0,0 +1,157 @@
+<?php namespace Tests\Unit;
+
+use Hampel\BannerTest\SubContainer\Banner;
+use Tests\TestCase;
+use XF\Phrase;
+
+class SubcontainerTest extends TestCase
+{
+	/** @var Banner */
+	protected $banner;
+
+	protected function setUp() : void
+	{
+		parent::setUp();
+
+		$this->banner = $this->app()->get('banner');
+	}
+
+	// ------------------------------------------------
+
+	public function test_initialisation()
+	{
+		$this->assertIsArray($this->banner->getColours());
+	}
+
+	public function test_getColourKeys_returns_array()
+	{
+		$colourKeys = $this->banner->getColourKeys();
+
+		$this->assertIsArray($colourKeys);
+		$this->assertTrue(in_array('black', $colourKeys));
+	}
+
+	public function test_getColour_returns_null()
+	{
+		$this->assertNull($this->banner->getColour(null));
+		$this->assertNull($this->banner->getColour(''));
+		$this->assertNull($this->banner->getColour(1));
+		$this->assertNull($this->banner->getColour('foo'));
+	}
+
+	public function test_getColour_returns_colour_array()
+	{
+		$colour = $this->banner->getColour('black');
+		$this->assertIsArray($colour);
+		$this->assertArrayHasKey('background', $colour);
+		$this->assertArrayHasKey('text', $colour);
+		$this->assertArrayHasKey('phrase', $colour);
+	}
+
+	public function test_getColoursForSelect_returns_array()
+	{
+		$colours = $this->banner->getColoursForSelect();
+		$this->assertIsArray($colours);
+		$this->assertArrayHasKey('black', $colours);
+		$this->assertInstanceOf(Phrase::class, $colours['black']);
+	}
+
+	public function test_getAbstractedDataPath()
+	{
+		$this->setOption('hampelBannerGeneratorSavePath', 'foo');
+
+		$path = $this->banner->getAbstractedDataPath(100, 50, 'bar');
+
+		$this->assertEquals('data://foo/100x50-bar.png', $path);
+	}
+
+	public function test_generateBanner_logs_error_for_no_colour()
+	{
+		$this->fakesErrors();
+
+		$this->banner->generateBanner(100, 50, 'foo');
+
+		$this->assertErrorLogged("Invalid colour specified for banner test: foo");
+	}
+
+	public function test_generateBanner_returns_null_for_existing_image()
+	{
+		$this->swapFs('data');
+		$this->setOption('hampelBannerGeneratorSavePath', 'foo');
+
+		$path = 'data://foo/100x50-red.png';
+
+		// check there's nothing there now
+		$this->assertFalse($this->app->fs()->has($path));
+
+		// create a file at our destination
+		$this->app->fs()->write($path, '');
+
+		$this->assertTrue($this->app->fs()->has($path));
+		$this->assertEquals(0, $this->app->fs()->getSize($path));
+
+		$this->assertEquals($path, $this->banner->generateBanner(100, 50, 'red'));
+
+		// check new file hasn't been created
+		$this->assertTrue($this->app->fs()->has($path));
+		$this->assertEquals(0, $this->app->fs()->getSize($path));
+	}
+
+	public function test_generateBanner_generates_image()
+	{
+		$this->swapFs('data');
+		$this->setOption('hampelBannerGeneratorSavePath', 'foo');
+
+		$path = 'data://foo/100x50-red.png';
+
+		// check there's nothing there now
+		$this->assertFalse($this->app->fs()->has($path));
+
+		$this->assertEquals($path, $this->banner->generateBanner(100, 50, 'red'));
+
+		// check new file has been created
+		$this->assertTrue($this->app->fs()->has($path));
+		$this->assertEquals(180, $this->app->fs()->getSize($path));
+	}
+
+	public function test_generateBanner_generates_default_image()
+	{
+		$this->swapFs('data');
+		$this->setOption('hampelBannerGeneratorSavePath', 'foo');
+
+		$path = 'data://foo/100x50-green.png';
+
+		// check there's nothing there now
+		$this->assertFalse($this->app->fs()->has($path));
+
+		$this->assertEquals($path, $this->banner->generateBanner(100, 50));
+
+		// check new file has been created
+		$this->assertTrue($this->app->fs()->has($path));
+		$this->assertEquals(180, $this->app->fs()->getSize($path));
+	}
+
+	public function test_generateBanner_generates_new_image_when_forced()
+	{
+		$this->swapFs('data');
+		$this->setOption('hampelBannerGeneratorSavePath', 'foo');
+
+		$path = 'data://foo/100x50-red.png';
+
+		// check there's nothing there now
+		$this->assertFalse($this->app->fs()->has($path));
+
+		// create a file at our destination
+		$this->app->fs()->write($path, '');
+
+		// check there's nothing there now
+		$this->assertTrue($this->app->fs()->has($path));
+		$this->assertEquals(0, $this->app->fs()->getSize($path));
+
+		$this->assertEquals($path, $this->banner->generateBanner(100, 50, 'red', true));
+
+		// check new file has been created
+		$this->assertTrue($this->app->fs()->has($path));
+		$this->assertEquals(180, $this->app->fs()->getSize($path));
+	}
+}
