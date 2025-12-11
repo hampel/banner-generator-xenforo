0d6c653d363afd5fd7facc8e01d6d9bed6d72d53
Simon Hampel <simon@hampelgroup.com>
2025-12-11 12:25:10 +1100

    bump version to 1.1.1

---
+++ _output/option_hint.php
@@ -0,0 +1,20 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property string|null $hampelBannerGeneratorDefaultClasses Default CSS classes
+ * @property string|null $hampelBannerGeneratorDefaultColour Default banner colour
+ * @property string|null $hampelBannerGeneratorSavePath Banner Save Path
+ */
+class Options
+{
+}
